211740b7867cfb69379562d42745d5f898c93120
wongd22 <55171651+wongd22@users.noreply.github.com>
2025-06-02 16:22:19 +0800

    Delete project-bolt-sb1-rizztx5z (1)/project directory

--- project-bolt-sb1-rizztx5z (1)/project/src/lib/firebase/index.js
+++
@@ -1,6 +0,0 @@
-// Export all Firebase functionality
-export * from './config';
-export * from './models/trade';
-export * from './models/user';
-export * from './models/account';
-export * from './models/note';
