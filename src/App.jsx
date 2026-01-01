8254999101cee1aed7a32c4d5c64d06a6d5d136d
derekwtwong <derekwtwong@gmail.com>
2026-01-01 08:03:17 +0000

    feat(routing): add place route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyPage from './pages/MyPage';
 import Shopping from './pages/Shopping';
+import Place from './pages/Place';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MyPage />} />
         <Route path="/shopping" element={<Shopping />} />
+        <Route path="/place" element={<Place />} />
       </Routes>
     </Router>
   );
